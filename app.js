7e80b6dbc17fe7e69aa32749278d57f84b318385
Bernardo Lopez <bernardolr17@gmail.com>
2021-12-30 22:12:07 -0600

    Filter bug corrected

--- app.js
+++ app.js
@@ -208,6 +208,7 @@ function deleteCheck(e){
     if(item.classList[0] === "complete-btn"){
         const todo = item.parentElement;
         todo.classList.toggle("completed");
+        
     }
 
 }
@@ -240,30 +241,6 @@ function filterTodo(e){
     })
 }
 
-
-function filterTodo(e) {
-    const todos = todoList.childNodes;
-    todos.forEach(function(todo) {
-      switch (e.target.value) {
-        case "all":
-          todo.style.display = "flex";
-          break;
-        case "completed":
-          if (todo.classList.contains("completed")) {
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-        case "uncompleted":
-          if (!todo.classList.contains("completed")) {
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-      }
-    });
-  }
 
 function saveLocalTodos(todo){
     //CHECK-- TO SEE IF THERE ARE THINGS ALREADY THERE
